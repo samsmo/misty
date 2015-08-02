21a263cfc118c8c23c5e121c6c92e18234772d39
Sam Smolenski <smmolenski@hugeinc.com>
2015-08-01 20:59:11 -0400

    inital setup

--- libs/app.jsx
+++ libs/app.jsx
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import ToolBar from './components/tool-bar.jsx';
 import Canvas from './components/canvas.jsx';
 
+// Not react components.
+import Pencil from './tools/pencil.js';
+
 import "./main.less";
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: '#FBDACB',
+            tool: Pencil,
+            history: {},
+        };
+    }
+
     render() {
         return (
             <div id="misty">
--- libs/components/tool-bar.jsx
+++ libs/components/tool-bar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class ToolBar extends Component {
     render() {
         return (
-            <section>Friday was pizza day</section>
+            <section>[ Tools go here ]</section>
         );
     }
 }
